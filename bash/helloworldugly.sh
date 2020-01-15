e9cd166e4eda5b8a2a7abb71569ee59fe7e00fe0
Lovepreet Singh <punjabiwifi@gmail.com>
2020-01-15 14:04:19 -0800

    Correction to helloworldugly

--- bash/helloworldugly.sh
+++ bash/helloworldugly.sh
@@ -4,10 +4,9 @@
 
 #This is a silly way of creating the output text by starting with something else and stream edit
 echo -n "helb wold" |
-  sed -e "s/b/o/g" -e "s/l/ll" -e "s/ol/orl" |
-  tr "h" "H" |
-  tr "w" "W"
+  sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |
+  tr "h" "H"|
+  tr "w" "W"|
   awk '{print $1 "\x20" $2 "\41"}'
-bc <<< "(($$ * 4 -24)/2 + 12)/2"  |
+bc <<< "(($$ * 4 - 24)/2 + 12)/2" |
   sed 's/^/I am process # /'
-  
