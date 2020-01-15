cbacca587a821f7878c5e2c69d2cf12a48b864f3
Lovepreet Singh <punjabiwifi@gmail.com>
2020-01-15 13:56:46 -0800

    doing third script

---
+++ bash/helloworldugly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# My third script - helloworldugly.#!/bin/sh
+# This script display the string "Hello World!"
+
+#This is a silly way of creating the output text by starting with something else and stream edit
+echo -n "helb wold" |
+  sed -e "s/b/o/g" -e "s/l/ll" -e "s/ol/orl" |
+  tr "h" "H" |
+  tr "w" "W"
+  awk '{print $1 "\x20" $2 "\41"}'
+bc <<<  
