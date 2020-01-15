54faf239c6856a39468257fd996fbddabcca1821
Lovepreet Singh <punjabiwifi@gmail.com>
2020-01-15 14:56:53 -0800

    Finished task 2 in the script

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -26,6 +26,6 @@ echo ""
 
 echo "Setgid files:"
 echo "============="
-
-find / -type f -executable -pem -2000 -ls 2>/dev/null | sort -k 3
+find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 4
 echo ""
+
