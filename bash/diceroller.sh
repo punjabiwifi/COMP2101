cb7cd41e61cdd5a673812eda09ac7367dc500abe
Lovepreet Singh <punjabiwifi@gmail.com>
2020-01-22 18:29:53 -0800

    Edited script to roll dice with 9 faces as my first name letter count

---
+++ bash/diceroller.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# this script rolls a six-sided die
+#
+
+# Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
+
+# roll the dice and display the result
+echo "
+Rolling...
+$(( RANDOM % 6 + 1)) rolled
+"
+
+#Dice that rolls as many dice in my first name, my name is Lovepreet
+echo "
+Rolling dice with 9 faces
+$(( RANDOM % 9+1))rolled
+"
