129aaf543237558615c05a9a7b20abda84372a61
Lovepreet Singh <punjabiwifi@gmail.com>
2020-01-22 18:43:51 -0800

    Added the 5 and 20 faced dices

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -5,14 +5,26 @@
 
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
-# roll the dice and display the result
+# roll the six faced dice and display the result
 echo "
-Rolling...
+Rolling dice with six faces...
 $(( RANDOM % 6 + 1)) rolled
 "
 
-#Dice that rolls as many dice in my first name, my name is Lovepreet
+#roll the five faced dice and display the result
 echo "
-Rolling dice with 9 faces
-$(( RANDOM % 9+1))rolled
+Rolling dice with 5 faces...
+$(( RANDOM % 5 +1)) rolled
+"
+#roll the 20 faced dice and display the result
+echo "
+Rolling dice with 20 faces...
+$(( RANDOM % 20 +1)) rolled
+"
+
+
+#Dice that rolls as many letters in my first name, my name is Lovepreet
+echo "
+Rolling dice with 9 faces...
+$(( RANDOM % 9 +1)) rolled
 "
