d710d5147c4a5746a28ea5777a8005eed2e61912
Lovepreet Singh <punjabiwifi@gmail.com>
2020-01-24 19:43:01 -0800

    Finished the arithmetic file

---
+++ bash/arithmetic-demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# this script demonstrates doing arithmetic
+
+# Task 1: Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user.
+# Task 2: Change the output to only show:
+#    the sum of the 3 numbers with a label
+#    the product of the 3 numbers with a label
+
+read -p "Please enter the first number - " myvar1
+read -p "Please enter the second number - " myvar2
+read -p "Please enter the third number - " myvar3
+
+sum=$((myvar1 + myvar2 + myvar3 ))
+product=$((myvar1*myvar2*myvar3))
+
+cat <<EOF
+$myvar1 plus $myvar2 plus $myvar3 is $sum
+$myvar1 multiplied by $myvar2 multiplied by $myvar3 is $product
+EOF
